2b7dc087215c79172e66f6af09a639453ef5810b
katherineylu <148897977+katherineylu@users.noreply.github.com>
2023-11-07 12:17:44 +0100

    Massive update (#15)
    problems with team and contact pages still

--- scripts/script.js
+++ scripts/script.js
@@ -1,37 +1,3 @@
-/*
-  This is your site JavaScript code - you can add interactivity!
-*/
-
-// Print a message in the browser's dev tools console each time the page loads
-// Use your menus or right-click / control-click and choose "Inspect" > "Console"
-console.log("Hello 🌎");
-
-/* 
-Make the "Click me!" button move when the visitor clicks it:
-- First add the button to the page by following the steps in the TODO 🚧
-*/
-const btn = document.querySelector("button"); // Get the button from the page
-if (btn) { // Detect clicks on the button
-  btn.onclick = function () {
-    // The 'dipped' class in style.css changes the appearance on click
-    btn.classList.toggle("dipped");
-  };
-}
-
-
-// ----- GLITCH STARTER PROJECT HELPER CODE -----
-
-// Open file when the link in the preview is clicked
-let goto = (file, line) => {
-  window.parent.postMessage(
-    { type: "glitch/go-to-line", payload: { filePath: file, line: line } }, "*"
-  );
-};
-// Get the file opening button from its class name
-const filer = document.querySelectorAll(".fileopener");
-filer.forEach((f) => {
-  f.onclick = () => { goto(f.dataset.file, f.dataset.line); };
-});
 
 
 let slideIndex = 0;
@@ -45,6 +11,44 @@ function showSlides() {
   }
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
+  if (slides[slideIndex-1])
+    slides[slideIndex-1].style.display = "block";
   setTimeout(showSlides, 6000); // Change image every 2 seconds
+}
+
+const filterGlasses = (category) => {
+    const filterButtons = document.getElementsByClassName('filter-button');
+    const selectedElements = document.getElementsByClassName(category);
+    const selectedButton = document.getElementById('btn-' + category);
+    const isActive = selectedButton.classList.contains('active');
+    if (!isActive) {
+        for (let button of filterButtons) {
+            button.classList.remove('active');
+        }
+        selectedButton.classList.add('active');
+        showAll();
+        for (let element of selectedElements) {
+            element.style.display = 'none';
+        }
+    }
+}
+
+const showAllGlasses = () => {
+    const filterButtons = document.getElementsByClassName('filter-button');
+    const selectedButton = document.getElementById('btn-all');
+    const isActive = selectedButton.classList.contains('active');
+    if (!isActive) {
+        for (let button of filterButtons) {
+            button.classList.remove('active');
+        }
+        selectedButton.classList.add('active');
+        showAll();
+    }
+}
+
+const showAll = () => {
+    const allElements = document.getElementsByClassName('glasses-item');
+    for (let element of allElements) {
+        element.style.display = 'flex';
+    }
 }
